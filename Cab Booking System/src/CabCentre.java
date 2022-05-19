f497caf14e0657a7a26c75c9e98bcb40309fda35
poojappd <poojapriyadarshnips@gmail.com>
2022-05-19 05:48:01 +0530

    Morning changes

---
+++ Cab Booking System/src/CabCentre.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class CabCentre {
+     private StationPoint locatedStationPoint;
+     private HashMap<VehicleType, Vehicle> availableVehicles;
+     private HashMap<String, Driver> availableDrivers;
+
+      void addVehicle(Vehicle newVehicle, Driver newDriver){
+          newVehicle.
+          newVehicle.getVehicleType()
+      }
+
+      void addVehicle(Vehicle newVehicle)
+}
